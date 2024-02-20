f92afd20c2a7ce1b58eaaeab6f6d4954b534981c
Crowdbotics <team@crowdbotics.com>
2024-02-20 16:46:52 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wandering-violet-46500",
+  url: "https://wandering-violet-46500.botics.co",
+  api: "https://wandering-violet-46500.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
